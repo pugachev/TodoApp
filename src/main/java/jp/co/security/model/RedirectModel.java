a43adf1655e59f6c6f7341d8085081b61c5dbe0b
mtake <mtake@10.0.1.121>
2018-05-26 16:04:55 +0900

    データモデル追加
    ・コントローラ内部でリダイレクトするために利用するためのデータクラスを追加

---
+++ src/main/java/jp/co/security/model/RedirectModel.java
@@ -0,0 +1,27 @@
+package jp.co.security.model;
+
+public class RedirectModel
+{
+	String username;
+	String isSearch;
+
+	public String getUsername()
+	{
+		return username;
+	}
+
+	public void setUsername(String username)
+	{
+		this.username = username;
+	}
+
+	public String getIsSearch()
+	{
+		return isSearch;
+	}
+
+	public void setIsSearch(String isSearch)
+	{
+		this.isSearch = isSearch;
+	}
+}
